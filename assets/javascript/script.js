dd158b2e6a0d43521975d0f8c6e2b3613b6c0c99
Meet Kalani <meetkalani2002@gmail.com>
2024-02-17 14:46:51 +0530

    add object literals and handle empty string as input

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -26,42 +26,54 @@ document.addEventListener('DOMContentLoaded', () => {
 function getData(location) {
     try {
         let userInputData = location.trim();
-        if (userInputData === "") {
-            warningElement.style.visibility = "visible";
-        } else {
-            warningElement.style.visibility = "hidden";
-        }
+
+        warningElement.style.visibility = (!userInputData) ? "visible" : "hidden";
+        if(!userInputData) return;
 
         let xhr = new XMLHttpRequest();
 
-        xhr.open('GET', 'http://api.weatherapi.com/v1/current.json?key=cf8f0b2edfe540168fe84553241602&q=' + userInputData);
+        xhr.open('GET', `http://api.weatherapi.com/v1/current.json?key=cf8f0b2edfe540168fe84553241602&q=${userInputData}`); 
 
         xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
 
         xhr.responseType = 'json';
 
         xhr.onload = () => {
+            console.log(xhr.response)
             if (xhr.status > 300) {
                 // location not found
                 if (xhr.response.error.code === 1006) {
                     userInput.value = "";
                     alert('The location you entered could not be found. Please try again.');
                 }
+                return;
             }
 
-            iconElement.src = xhr.response.current.condition.icon;
-            weatherTypeElement.textContent = xhr.response.current.condition.text;
-            locationElement.textContent = xhr.response.location.name;
-            stateElement.textContent = xhr.response.location.region + ",";
-            countryElement.textContent = xhr.response.location.country;
-            temperatureCelsiusElement.textContent = xhr.response.current.temp_c + "°C/ ";
-            temperatureFahrenheitElement.textContent = xhr.response.current.temp_f + "°F";
-            temperatureFeelElement.textContent = "Feels Like: " + xhr.response.current.feelslike_c + "°C";
-            windElement.textContent = "Wind: " + xhr.response.current.wind_kph + " KMPH";
-            humidityElement.textContent = "Humidity: " + xhr.response.current.humidity;
+            const {
+                current: {
+                    condition: { icon, text },
+                    temp_c,
+                    temp_f,
+                    feelslike_c,
+                    wind_kph,
+                    humidity
+                },
+                location: { name, region, country, localtime }
+            } = xhr.response;
 
-            const dateObject = new Date(xhr.response.location.localtime);
-            const timeString = dateObject.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12:true });
+            iconElement.src = icon;
+            weatherTypeElement.textContent = text;
+            locationElement.textContent = name;
+            stateElement.textContent = `${region},`;
+            countryElement.textContent = country;
+            temperatureCelsiusElement.textContent = `${temp_c}°C/`;
+            temperatureFahrenheitElement.textContent = `${temp_f}°F`;
+            temperatureFeelElement.textContent = `Feels Like: ${feelslike_c}°C`;
+            windElement.textContent = `Wind: ${wind_kph} KMPH`;
+            humidityElement.textContent = `Humidity: ${humidity}`;
+
+            const dateObject = new Date(localtime);
+            const timeString = dateObject.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true });
 
             timeElement.textContent = timeString;
 
